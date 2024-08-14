f67da0e7f9b440949b70769a4c7ef8e5d6fe0775
HuuDungg <huudung2004kt@gmail.com>
2024-08-14 17:27:44 +0700

    render list in react

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import InputTodoComponent from './components/input.todo'
 
 function App() {
   const myName: string = 'hi there';
+  const myAge: number = 34
   return (
     <>
       <InputTodoComponent
         myName={myName}
+        myAge={myAge}
       >
       </InputTodoComponent>
     </>
--- src/components/input.todo.tsx
+++ src/components/input.todo.tsx
@@ -1,12 +1,25 @@
 interface myProps {
     myName: string
+    myAge: number
 }
 const InputTodoComponent = (props: myProps) => {
+    const todos = ["todo 1", "todo 2", "todo 1", "todo 2", "todo 1", "todo 2", "todo 1", "todo 2",]
+    const { myName, myAge } = props
     return (
         <>
-            <label htmlFor="">Input your todo {props.myName}</label> <br />
+            <label htmlFor="">Input your todo {myName} an my age {myAge}</label> <br />
             <input type="text" /> <br />
             <button>Save</button>
+
+            <ul>
+                {todos.map((items, index) => {
+                    return (
+                        <>
+                            <li key={index + "lew"}>{items}</li>
+                        </>
+                    )
+                })}
+            </ul>
         </>
     )
 }
