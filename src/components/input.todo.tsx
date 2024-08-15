d4d02c561131dd80c6a06e622201933333921dca
HuuDungg <huudung2004kt@gmail.com>
2024-08-15 08:13:33 +0700

    handle some event

--- src/components/input.todo.tsx
+++ src/components/input.todo.tsx
@@ -1,15 +1,30 @@
+import { useState } from "react"
+
 interface myProps {
     myName: string
     myAge: number
 }
 const InputTodoComponent = (props: myProps) => {
-    const todos = ["todo 1", "todo 2", "todo 1", "todo 2", "todo 1", "todo 2", "todo 1", "todo 2",]
+    const [todos, setTodos] = useState(["todo 1", "todo 2", "todo 1", "todo 2", "todo 1", "todo 2", "todo 1", "todo 2"])
     const { myName, myAge } = props
+    const [mineName, setMineName] = useState<string>("")
+    const handleTyping = (name: string) => {
+        setMineName(name)
+    }
+    const handleClick = () => {
+        setTodos([...todos, mineName])
+        setMineName('')
+    }
     return (
         <>
             <label htmlFor="">Input your todo {myName} an my age {myAge}</label> <br />
-            <input type="text" /> <br />
-            <button>Save</button>
+            <div>Name input with state {mineName}</div>
+            <input type="text" value={mineName} onChange={(event) => {
+                handleTyping(event.target.value)
+            }} /> <br />
+            <button
+                onClick={() => { handleClick() }}
+            >Save</button>
 
             <ul>
                 {todos.map((items, index) => {
