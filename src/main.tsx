377d0e1cf39e08e4832a3e3d9494f0d1e3ffb9ea
HuuDungg <huudung2004kt@gmail.com>
2024-08-15 10:59:14 +0700

    setup react router(client render)

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <div>
+          <h1>Hello World</h1>
+          <Link to={'/home'}>Home visit</Link>
+        </div>
+      ),
+    },
+    {
+      path: "/home",
+      element: <App></App>
+    },
+  ]
+);
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
