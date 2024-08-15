9080258a8d2ae1bbbd4b98887ada5a612f74869b
HuuDungg <huudung2004kt@gmail.com>
2024-08-15 11:26:13 +0700

    use antd component to design ui

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import {
   Route,
   Link,
 } from "react-router-dom";
+import UserTable from './components/user/user.table.tsx';
 
 const router = createBrowserRouter(
   [
@@ -16,13 +17,28 @@ const router = createBrowserRouter(
       element: (
         <div>
           <h1>Hello World</h1>
-          <Link to={'/home'}>Home visit</Link>
+          <Link to={'/home'}>Home visit</Link> <br />
+          <Link to={'/users'}>users visit</Link><br />
+          <Link to={'/subjects'}>subject visit</Link><br />
+          <Link to={'/students'}>student visit</Link><br />
         </div>
       ),
     },
     {
       path: "/home",
       element: <App></App>
+    },
+    {
+      path: "/users",
+      element: <UserTable />
+    },
+    {
+      path: "/subjects",
+      element: <div>this is users subjcets</div>
+    },
+    {
+      path: "/students",
+      element: <div>this is users students</div>
     },
   ]
 );
